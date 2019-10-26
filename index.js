c1120f0a7c82eed955117a07d3ce89659faf3b61
Alex <alexpetro000@gmail.com>
2019-10-26 12:17:15 +0300

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const autossh = require('autossh');
+const fs = require('fs');
+
+const port = fs.readFileSync('/boot/port', 'utf8').trim();
+console.log('PORT: ', port);
+
+let postPublicKey_once = false;
+
+autossh({
+    host: 'd.qtech.pw',
+    username: 'sshtun',
+    localPort: 22,
+    remotePort: port,
+    reverse: true,
+    serverAliveInterval: 10,
+    serverAliveCountMax: 2
+})
+.on('error', err => {
+    console.error('ERROR: ', err);
+
+    if(!postPublicKey_once) {
+        postPublicKey_once = true;
+
+        console.log('Trying to post public key to server...');
+        postPublicKey();
+    }
+})
+.on('connect', connection => {
+    console.log('connection pid: ' + connection.pid);
+});
+
+function postPublicKey() {
+    var req = request.post('http://d.qtech.pw/remote', (err, resp, body) => {
+        if (err) {
+            console.log('Error!');
+        } else {
+            console.log('URL: ' + body);
+        }
+    });
+    var form = req.form();
+    form.append('file', '<FILE_DATA>', {
+        filename: '/home/pi/.ssh/id_rsa/id_rsa.pub',
+        contentType: 'text/plain'
+    });
+}
